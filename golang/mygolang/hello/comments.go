e28f1b48b65315dd2fb0fcb3e2c6fb9e55c04704
Shubham <iamshubham316@gmail.com>
2022-08-03 23:07:13 +0530

    variables

---
+++ golang/mygolang/hello/comments.go
@@ -0,0 +1,9 @@
+package main
+import ("fmt")
+
+func main2() { //change from main2 to main before running
+  //this is a comment
+  /*this is a multi line comment
+  */
+  fmt.Println("Some Comments")
+}
---
+++ golang/mygolang/variables/variable.go
@@ -0,0 +1,11 @@
+package main
+import("fmt")
+
+func main(){
+	var fname string = "Shubham"
+	var lname= "Tandon"
+	fnum:=2
+	fmt.Println(fname)
+	fmt.Println(lname)
+	fmt.Println(fnum)
+}
